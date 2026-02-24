1a634c9b428f083288ca22f811b830c626c553f5
pres <mateospres@gmail.com>
2026-02-23 23:40:19 -0400

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ancbprancheta/sw.js', { scope: '/ancbprancheta/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
